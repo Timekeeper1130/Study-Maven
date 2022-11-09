b18ade21378bcf99ff5ba41888a1e93696eaed5d
Timekeeper <632561223@qq.com>
2022-11-09 22:12:33 +0800

    init

---
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Timekeeper
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+}
